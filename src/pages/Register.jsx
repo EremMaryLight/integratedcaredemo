5b40c44121b140df7e220e0f90c463a8b192c819
EremMaryLight <eremmary24@gmail.com>
2024-01-27 18:43:36 +0100

    newpag

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -15,16 +15,36 @@ export default function Register() {
           <section>
             <div>
               <form>
-                <label>First Name</label>
-                <input/>
-                <label>Last Name</label>
-                <input/>
-                <label>Email Address</label>
-                <input/>
-                <label>Password</label>
-                <input/>
+                
+<div class=" relative ">
+    <label for="name-with-label" class="text-gray-700">
+        First Name
+    </label>
+    <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="firstname" placeholder="firstname"/>
+    </div>
+
+                
+<div class=" relative ">
+    <label for="name-with-label" class="text-gray-700">
+        Last Name
+    </label>
+    <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="lastname" placeholder="Your name"/>
+    </div>
+
+    <div class=" relative ">
+    <label for="name-with-label" class="text-gray-700">
+        Email
+    </label>
+    <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="lastname" placeholder="email"/>
+    </div>
+    <div class=" relative ">
+    <label for="name-with-label" class="text-gray-700">
+        Password
+    </label>
+    <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="lastname" placeholder="Your name"/>
+    </div>
                 <input type="checkbox"/><p>I agree to the terms of service and privacy policy</p>
-                <button>
+                <button className="mt-6" fullWidth>
                   Sign Up
                 </button>
                 <p>Already have an account? <Link to ='Login'>Login</Link></p>
@@ -32,6 +52,9 @@ export default function Register() {
             </div>
           </section>
          </div>
+
+
+         
          
       </>
   );
