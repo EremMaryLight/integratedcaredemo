d217e77d500fef568af512db2ca64c413717560c
EremMaryLight <eremmary24@gmail.com>
2024-01-27 18:17:41 +0100

    newpage

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -16,10 +16,18 @@ export default function Login() {
             <div>
               <form>
                 
-                <label>Email Address</label>
-                <input/>
-                <label>Password</label>
-                <input/>
+              <div class=" relative ">
+    <label for="name-with-label" class="text-gray-700">
+        Email
+    </label>
+    <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="lastname" placeholder="email"/>
+    </div>
+    <div class=" relative ">
+    <label for="name-with-label" class="text-gray-700">
+        Password
+    </label>
+    <input type="text" id="name-with-label" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="lastname" placeholder="Your name"/>
+    </div>
                 <input type="checkbox"/><p>I agree to the terms of service and privacy policy</p>
                 <button>
                    Login
@@ -27,6 +35,8 @@ export default function Login() {
                 <p>Do not have an account? <Link to ='./Register'>Register</Link></p>
               </form>
             </div>
+
+
           </section>
          </div>
          
